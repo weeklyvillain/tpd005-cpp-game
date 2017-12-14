8ea1dc6675457c4ffac7db7ccf014725f766dd32
Jimmy <jimbj685@student.liu.se>
2017-12-14 11:22:01 +0100

    More refactoring

--- source/objects/imps/Menu.cpp
+++ source/objects/imps/Menu.cpp
@@ -11,17 +11,20 @@ Menu::Menu(float width, float height) : selectedItemIndex{0}, font{}
 	menu[0].setFont(font);
 	menu[0].setColor(sf::Color::Red);
 	menu[0].setString("Play");
-	menu[0].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
+	sf::FloatRect item = menu[0].getGlobalBounds();
+	menu[0].setPosition(sf::Vector2f((width / 2) - (item.width / 2), height / (MAX_NUMBER_OF_ITEMS + 1) * 1));
 
 	menu[1].setFont(font);
 	menu[1].setColor(sf::Color::White);
 	menu[1].setString("Options");
-	menu[1].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
+	item = menu[1].getGlobalBounds();
+	menu[1].setPosition(sf::Vector2f((width / 2) - (item.width / 2), height / (MAX_NUMBER_OF_ITEMS + 1) * 2));
 
 	menu[2].setFont(font);
 	menu[2].setColor(sf::Color::White);
 	menu[2].setString("Exit");
-	menu[2].setPosition(sf::Vector2f(width / 2, height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
+	item = menu[2].getGlobalBounds();
+	menu[2].setPosition(sf::Vector2f((width / 2) - (item.width / 2), height / (MAX_NUMBER_OF_ITEMS + 1) * 3));
 
 	selectedItemIndex = 0;
 }
@@ -46,6 +49,10 @@ void Menu::MoveUp()
 		menu[selectedItemIndex].setColor(sf::Color::White);
 		selectedItemIndex--;
 		menu[selectedItemIndex].setColor(sf::Color::Red);
+	} else {
+		menu[selectedItemIndex].setColor(sf::Color::White);
+		selectedItemIndex = 2;
+		menu[selectedItemIndex].setColor(sf::Color::Red);
 	}
 }
 
@@ -55,6 +62,10 @@ void Menu::MoveDown()
 	{
 		menu[selectedItemIndex].setColor(sf::Color::White);
 		selectedItemIndex++;
+		menu[selectedItemIndex].setColor(sf::Color::Red);
+	} else {
+		menu[selectedItemIndex].setColor(sf::Color::White);
+		selectedItemIndex = 0;
 		menu[selectedItemIndex].setColor(sf::Color::Red);
 	}
 }
